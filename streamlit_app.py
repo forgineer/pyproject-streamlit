e67098c8abc23c6d92f3d3769bf29845a4be5bc3
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2024-11-06 22:15:36 -0600

    Project setup with initial pyproject file and main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome to PyProject Builder!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "More coming soon..."
 )
